a95a7232b676e7d558469a02de9cb5059130c2b1
emil14 <emil.musician@gmail.com>
2019-02-04 11:26:11 +0300

    Minor refactor of app init

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,10 @@
 import os
 from flask import Flask
 
-SECRET_KEY = os.getenv("FLASK_SECRET_KEY")
-
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_mapping(SECRET_KEY=SECRET_KEY)
+    app.config.from_mapping(SECRET_KEY=os.getenv('FLASK_SECRET_KEY'))
 
     @app.route('/')
     def handle_root():
