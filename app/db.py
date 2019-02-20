459747e48166d49faea276f4fbfc9309f5509f25
emil14 <emil.musician@gmail.com>
2019-02-20 17:48:20 +0300

    Add first SQLAlchemy Class

--- app/db.py
+++ app/db.py
@@ -1,4 +1,12 @@
 import os
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine(os.getenv('FLASK_DB_URI'))
+Base = declarative_base()
+
+
+class Type(Base):
+    __tablename__ = 'types'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
